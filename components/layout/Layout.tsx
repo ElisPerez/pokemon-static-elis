15f878be2baca21636974d3243dfafc7a197c9f9
Elis Antonio Perez <elisperezmusic@gmail.com>
2022-09-19 10:17:22 -0500

    window.location.origin para obtener el domain name

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -5,7 +5,12 @@ interface Props {
   children?: React.ReactNode;
   title?: string;
 }
+
+// URL of front-end, domain name:
+const origin = (typeof window === 'undefined') ? '' : window.location.origin;
+
 export const Layout: React.FC<Props> = ({ children, title }) => {
+  // console.log(origin);
   return (
     <>
       <Head>
@@ -13,6 +18,13 @@ export const Layout: React.FC<Props> = ({ children, title }) => {
         <meta name='author' content='Elis Antonio Perez' />
         <meta name='description' content={`Info about Pokemon ${title}`} />
         <meta name='keywords' content={`${title}, pokemon, pokedex`} />
+
+        <meta property='og:title' content={title} />
+        <meta property='og:description' content={`Info about ${title}`} />
+        <meta
+          property='og:image'
+          content={`${origin}/img/banner.png`}
+        />
       </Head>
 
       <Navbar />
